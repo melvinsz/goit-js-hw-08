9453847f1bf94a70d50487576c7d89cafcdc3ad9
Stanislav <Stas.zaliiski@gmail.com>
2023-03-06 12:48:37 +0100

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const palletteContainer = document.querySelector('.gallery');
+const cardPictures = createPicturesCards(galleryItems);
+palletteContainer.insertAdjacentHTML('beforeend', cardPictures);
+
+function createPicturesCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" /> </a></li>`;
+    })
+    .join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: '250',
+});
